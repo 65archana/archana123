3e8599b00d1c8fbd1f8e873d15fe44dab383a167
65archana <47095433+65archana@users.noreply.github.com>
2019-01-30 18:04:15 +0530

    Add files via upload

---
+++ exp.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+fs=8000
+f=1000
+x=np.arange(-1,1,0.02)
+plt.figure(1)
+y=np.exp(x)
+
+plt.plot(x,y,'red')
+
+
+
+plt.xlabel('sample(n)')
+plt.ylabel('voltage(v)')
+plt.show()
